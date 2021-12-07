0b0de2c7fe0c1cf1198dfc638b9f60e99e4456d3
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-12-07 16:32:38 +0100

    Zmieniono sposób wyboru rozwiązań do rozmnażania Zamiast jednorodnego prawdopodobieństwa jest ważone jakością

--- genetic.py
+++ genetic.py
@@ -7,26 +7,31 @@ from typing import List, Tuple
 
 #
 # Populacja przechowywana jest jako lista par. Pierwszy element pary
-# to jakość rozwiązania, a drugi - tablica przypisująca zadanie do procesora.
+# to Cmax rozwiązania, a drugi - tablica przypisująca zadanie do procesora.
 # Ta realizacja pozwala na oszczędność w czasie działania i jest o ok. 70% szybsza
 # niż przechowywanie poszczególnych rozwiązań jako obiektów klasy Solution.
 #
+# Jakość a Cmax. Cmax jest chwilą, w której zakończy się ostatnie zadanie. Wobec
+# tego, pożądana jest minimalizacja tej wielkości. Jednak na potrzeby algorytmu
+# konieczne było wprowadzenie miary jakości, którą należy zmaksymalizować.
+# Dlatego za jakość przyjęto 1/Cmax
+#
 
 # Konfiguracja algorytmu
-GENE_MUTATION_CHANCE = 0.2          # Prawdopodobieństwo mutacji genu w rozwiązaniu, które mutuje
+GENE_MUTATION_CHANCE = 0.03         # Prawdopodobieństwo mutacji genu w rozwiązaniu, które mutuje
 CHILDREN_IN_ITERATION = 50          # Liczba dzieci w iteracji algorytmu
 MAX_DURATION = 300                  # Maksymalny czas pracy w sekundach
 MAX_ITERATIONS = 500                # Maksymalna liczba iteracji
 POPULATION_SIZE = 100               # Rozmiar populacji
-POPULATION_TO_CROSSOVER = 1         # Odsetek populacji, który się rozmnaża
+POPULATION_TO_DIE = 0.2             # Odsetek populacji, który zginie w iteracji
 RANDOM_SOLUTIONS = 0.5              # Odsetek losowych rozwiązań w pierwotnej populacji
-SOLUTION_MUTATION_CHANCE = 0.5      # Prawdopodobieństwo, że w rozwiązaniu zajdzie mutacja
+SOLUTION_MUTATION_CHANCE = 1        # Prawdopodobieństwo, że w rozwiązaniu zajdzie mutacja
 
 # Diagnostyka
 PRINT_STATS_FREQ = 10               # Co ile iteracji wyświetlać status
 
 # Zmienne związane z pracą programu
-best_qualities = []
+best_cmaxes = []
 iterations = 0
 start_time = 0.0
 
@@ -36,7 +41,7 @@ processor_count = 0
 
 # Punkt wejściowy algorytmu
 def genetic(proc_count: int, exec_times: List[int]) -> None:
-    global best_qualities, iterations, start_time
+    global best_cmaxes, iterations, start_time
     global execution_times, processor_count
 
     execution_times = exec_times
@@ -46,15 +51,15 @@ def genetic(proc_count: int, exec_times: List[int]) -> None:
     population = generateInitialSolutions(POPULATION_SIZE)
     sortPopulation(population)
     while canContinue():
-        doGeneticIteration(population)
-        best_qualities.append(population[0][0])
+        population = doGeneticIteration(population)
+        best_cmaxes.append(population[0][0])
         iterations += 1
         printStats()
 
 
-# Sprawdza czas trwania, jakość rozwiązania i ew. inne metryki i decyduje czy kontynuować
+# Sprawdza czas trwania, Cmax rozwiązania i ew. inne metryki i decyduje czy kontynuować
 def canContinue() -> bool:
-    global best_qualities, iterations, start_time
+    global best_cmaxes, iterations, start_time
     global MAX_DURATION, MAX_ITERATIONS
     duration = time() - start_time
 
@@ -76,12 +81,11 @@ def canContinue() -> bool:
 
 
 # Wykonuje iterację algorytmu genetycznego
-def doGeneticIteration(population: List[Tuple[int, List[int]]]) -> None:
-    initial_population_size = len(population)
-    performCrossOvers(population)
+def doGeneticIteration(population: List[Tuple[int, List[int]]]) -> List[Tuple[int, List[int]]]:
+    population = performCrossOvers(population)
     performMutations(population)
     sortPopulation(population)
-    removeWorstSolutions(population, initial_population_size)
+    return population
 
 
 # Generuje zestaw początkowych rozwiązań
@@ -94,8 +98,8 @@ def generateInitialSolutions(population_size: int) -> List[Tuple[int, List[int]]
             solution = [ randint(0, processor_count-1) for _ in range(process_count) ]
         else:
             solution = buildSolutionGreedy()
-        quality = measureSolutionQuality(solution)
-        population[i] = (quality, solution)
+        cmax = measureSolutionCmax(solution)
+        population[i] = (cmax, solution)
     return population
 
 
@@ -115,22 +119,38 @@ def buildSolutionGreedy() -> List[int]:
     return solution
 
 
-# Usuwa najgorsze rozwiązania z populacji, tak by przywrócić jej pierwotny rozmiar
-def removeWorstSolutions(population: List[Tuple[int, List[int]]], target_size: int) -> None:
-    while len(population) > target_size:
-        population.pop()
+# Wybiera rozwiązania do rozmnożenia i tworzy nową populację
+# Jeśli dzieci jest mniej niż rodziców, to nowa populacja jest
+# uzupełniana starą
+def performCrossOvers(population: List[Tuple[int, List[int]]]) -> List[Tuple[int, List[int]]]:
+    cross_overs = len(population) * POPULATION_TO_DIE
+    children = [None] * len(population)
+    pop_quality_sum = sumQualities(population)
 
-
-# Wybiera i rozmnaża rozwiązania, dodając je do populacji
-def performCrossOvers(population: List[Tuple[int, List[int]]]) -> None:
-    reproducible = int(POPULATION_SIZE * POPULATION_TO_CROSSOVER - 1e-9)
-    for _ in range(CHILDREN_IN_ITERATION):
-        parent1 = randint(0, reproducible)
-        parent2 = randint(0, reproducible - 1)
-        # Dba o to, by rozwiązanie nie rozmnażało się z samym sobą
-        parent2 += 1 if parent2 >= parent1 else 0
+    # Wypełnij tablicę dziećmi
+    idx = 0
+    while len(children) < cross_overs:
+        # Wybierz dwa różne rozwiązania do rozmnożenia
+        parent1 = select(population, pop_quality_sum)
+        parent2 = select(population, pop_quality_sum)
+        while parent1 == parent2:
+            parent2 = select(population, pop_quality_sum)
+        
         child = crossOver(population[parent1], population[parent2])
-        population.append(child)
+        children[idx] = child
+        idx += 1
+
+    # Uzupełnij populację osobnikami z poprzedniej
+    parents_to_survive = set()
+    while idx < len(children):
+        parent = select(population, pop_quality_sum)
+        if parent in parents_to_survive:
+            continue
+        parents_to_survive.add(parent)
+        children[idx] = population[parent]
+        idx += 1
+
+    return children
 
 
 # Rozmnaża rozwiązania
@@ -143,7 +163,7 @@ def crossOver(parent1: Tuple[int, List[int]], parent2: Tuple[int, List[int]]) ->
             offspring[i] = solution1[i]
         else:
             offspring[i] = solution1[i] if random() < 0.5 else solution2[i]
-    return (measureSolutionQuality(offspring), offspring)
+    return (measureSolutionCmax(offspring), offspring)
 
 
 # Wybiera rozwiązania i dokonuje mutacji
@@ -152,8 +172,9 @@ def performMutations(population: List[Tuple[int, List[int]]]) -> None:
     for i in range(len(population)):
         if random() >= SOLUTION_MUTATION_CHANCE:
             continue
-        new_solution = mutate(population[i])
-        population.append(new_solution)
+        population[i] = mutate(population[i])
+        #new_solution = mutate(population[i])
+        # population.append(new_solution)
 
 
 # Mutuje rozwiązanie i zwraca nową kopię
@@ -163,7 +184,7 @@ def mutate(solution: Tuple[int, List[int]]) -> Tuple[int, List[int]]:
     for i in range(len(solution)):
         if random() < GENE_MUTATION_CHANCE:
             new_solution[i] = randint(0, processor_count-1)
-    return (measureSolutionQuality(new_solution), new_solution)
+    return (measureSolutionCmax(new_solution), new_solution)
 
 
 # Sortuje populację od najlepszych rozwiązań
@@ -171,8 +192,8 @@ def sortPopulation(population: List[Tuple[int, List[int]]]) -> None:
     population.sort(key=lambda s: s[0])
 
 
-# Mierzy jakość rozwiązania (im mniej tym lepiej)
-def measureSolutionQuality(solution: List[int]) -> None:
+# Mierzy Cmax rozwiązania (im mniej tym lepiej)
+def measureSolutionCmax(solution: List[int]) -> None:
     global execution_times, processor_count
     processor_occupancy = [0] * processor_count
     for i in range(len(solution)):
@@ -181,18 +202,38 @@ def measureSolutionQuality(solution: List[int]) -> None:
     return max(processor_occupancy)
 
 
+# Sumuje jakości rozwiązań w populacji
+def sumQualities(population: List[Tuple[int, List[int]]]) -> float:
+    quality_sum = 0
+    for (cmax, _) in population:
+        quality_sum += 1 / cmax
+    return quality_sum
+
+
+# Zwraca indeks rozwiązania z puli
+def select(population: List[Tuple[int, List[int]]], quality_sum: float) -> int:
+    r = random() * quality_sum
+    for i in range(len(population)):
+        cmax = population[i][0]
+        r -= 1 / cmax
+        if r <= 0:
+            return i
+
+    return len(population) - 1
+
+
 # Wypisuje statystyki co określoną liczbę iteracji
 def printStats() -> None:
     if iterations % PRINT_STATS_FREQ != 0:
         return
     duration = round(time() - start_time, 1)
     digits = int(log10(MAX_ITERATIONS)) + 1
-    print(f'[{iterations: >{digits}}]: {duration: >6}s elapsed, Cmax: {best_qualities[-1]}')
+    print(f'[{iterations: >{digits}}]: {duration: >6}s elapsed, Cmax: {best_cmaxes[-1]}')
 
 
 # Wypisuje końcowe statystyki
 def printFinalStats() -> None:
-    global best_qualities, iterations, start_time
+    global best_cmaxes, iterations, start_time
     global execution_times, processor_count
     optimum = round(sum(execution_times) / processor_count, 1)
 
@@ -201,9 +242,9 @@ def printFinalStats() -> None:
     print('\nFinished job')
     print(f'    {iterations} iterations performed')
     print(f'    {duration} seconds')
-    print(f'    \033[1;32m{best_qualities[-1]} = Cmax\033[0m')
+    print(f'    \033[1;32m{best_cmaxes[-1]} = Cmax\033[0m')
     print(f'    {optimum} = Cmax* <divisible tasks>')
-    print(f'    {best_qualities[0]} -> {best_qualities[-1]} genetic progress')
+    print(f'    {best_cmaxes[0]} -> {best_cmaxes[-1]} genetic progress')
 
 
 def main() -> None:
